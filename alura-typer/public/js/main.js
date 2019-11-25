7c18ac925c7915abc4a05231bbc9ab1613fd4037
Percival <gabrielsilvaraujo@gmail.com>
2019-11-25 13:24:02 -0300

    Fim do curso de jquery1 e inicio do projeto das faculdades

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -83,10 +83,13 @@ function reiniciarJogo() {
 
 function inserePlacar() {
     let corpoTabela = $(".placar").find("tbody");
-    let numPalavras = $(".contador-palavras").text();
-    let botaoRemover = "<a href='#'><i class='small material-icons'>delete</i></a>";
     let usuario = "Gabriel";
+    let numPalavras = $(".contador-palavras").text();
     let linha = criaLinha(usuario, numPalavras);
+    linha.find(".botao-remover").click(function (event) {
+        event.preventDefault();
+        $(this).parent().parent().remove();
+    });
     corpoTabela.append(linha);
 }
 
@@ -98,12 +101,17 @@ function criaLinha(usuario, numPalavras) {
     let link = $("<a>").addClass("botao-remover").attr("href","#");
     let icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
 
-    link.append(icone)
+    link.append(icone);
+    colunaRemover.append(link);
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
 
-
+    console.log(linha);
+    return linha;
 }
 
 $(".botao-remover").click(function(event) {
-   event.preventDefault();
-   $(this).parent().parent().remove();
+    event.preventDefault();
+    $(this).parent().parent().remove();
 });
