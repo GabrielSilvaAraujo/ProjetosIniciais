bd2daf7020fd44acd17089a305302cdce83fb7f9
Percival <gabrielsilvaraujo@gmail.com>
2019-11-27 16:00:55 -0300

    Evolucao projeto faculdade e inicio curso jQuery 2

---
+++ ProjetoFaculdades/main.js
@@ -0,0 +1,50 @@
+$(function () {
+   removerTabela();
+});
+$("button").click(function (event) {
+    event.preventDefault();
+    inserePlacar();
+    $(".formulario").trigger("reset");
+
+});
+
+function criaLinha(faculdade, curso, tipo, periodo, preco, anotacoes) {
+    let linha = $("<tr>");
+    let colunaFaculdade = $("<td>").text(faculdade);
+    let colunaCurso = $("<td>").text(curso);
+    let colunaTipo = $("<td>").text(tipo);
+    let colunaPeriodo = $("<td>").text(periodo);
+    let colunaPreco = $("<td>").text(preco);
+    let colunaAnotacoes = $("<td>").text(anotacoes);
+
+    linha.append(colunaFaculdade);
+    linha.append(colunaCurso);
+    linha.append(colunaTipo);
+    linha.append(colunaPeriodo);
+    linha.append(colunaPreco);
+    linha.append(colunaAnotacoes);
+
+    return linha;
+}
+
+function inserePlacar() {
+    let corpoTabela = $(".tabela-informacoes").find("tbody");
+    let faculdade = $("#faculdade").val();
+    let curso = $("#curso").val();
+    let preco = $("#preco").val();
+    let anotacoes = $("#anotacoes").val();
+    let tipo = $("input[name='tipo']:checked").val();
+    let periodo = $("input[name='periodo']:checked").val();
+
+    let linha = criaLinha(faculdade, curso, tipo, periodo, preco, anotacoes);
+
+    corpoTabela.append(linha);
+}
+
+function removerTabela() {
+    let tabela = document.querySelector("table");
+
+    tabela.addEventListener("dblclick", function (event) {
+        event.target.parentNode.remove();
+    });
+}
--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -81,37 +81,3 @@ function reiniciarJogo() {
     campo.removeClass("campo-errado");
 }
 
-function inserePlacar() {
-    let corpoTabela = $(".placar").find("tbody");
-    let usuario = "Gabriel";
-    let numPalavras = $(".contador-palavras").text();
-    let linha = criaLinha(usuario, numPalavras);
-    linha.find(".botao-remover").click(function (event) {
-        event.preventDefault();
-        $(this).parent().parent().remove();
-    });
-    corpoTabela.append(linha);
-}
-
-function criaLinha(usuario, numPalavras) {
-    let linha = $("<tr>");
-    let colunaUsuario = $("<td>").text(usuario);
-    let colunaPalavras = $("<td>").text(numPalavras);
-    let colunaRemover = $("<td>");
-    let link = $("<a>").addClass("botao-remover").attr("href","#");
-    let icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
-
-    link.append(icone);
-    colunaRemover.append(link);
-    linha.append(colunaUsuario);
-    linha.append(colunaPalavras);
-    linha.append(colunaRemover);
-
-    console.log(linha);
-    return linha;
-}
-
-$(".botao-remover").click(function(event) {
-    event.preventDefault();
-    $(this).parent().parent().remove();
-});
